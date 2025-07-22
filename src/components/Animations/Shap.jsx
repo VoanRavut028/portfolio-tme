2cfb98b501f1b5cdf7c259f4c0d1aa526c786840
Visal <visalvai4@gmail.com>
2025-07-22 15:55:13 +0700

    modify

---
+++ src/components/Animations/Shap.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Shap = () => {
+  return (
+    <div>
+      <h2 className="text-red-700">Welcome to </h2>
+      <p>Part 2</p>
+      <p>part 3</p>
+    </div>
+  );
+};
+
+export default Shap;
