81ec4055d889d36b7f6230466b33827b45d89594
Visal <visalvai4@gmail.com>
2025-07-22 15:08:03 +0700

    Animate

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import AnimatedBackground from "./components/Background/Animationbackground";
-import Card from "./components/Card/CardComponent";
-import Home from "./components/Home/HomePage";
+
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
@@ -9,8 +8,8 @@ function App() {
       <div>
         <Navbar></Navbar>
         <AnimatedBackground></AnimatedBackground>
-        <Home></Home>
-        <Card></Card>
+        
+
       </div>
     </>
   );
